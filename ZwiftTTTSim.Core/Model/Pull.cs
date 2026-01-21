75440942abb25c26a9124384ff9e7f2eaa6f0092
Simone Chiaretta <simone@piyosailing.com>
2026-01-21 21:41:42 +0100

    Refactor team time trial simulation: - Remove Helpers class and its tests. - Introduce Pull and PullPosition models to represent rotations and rider positions. - Implement RotationComposer to create a list of Pulls based on RiderPowerPlans and rotations. - Update RiderPowerPlan to include essential rider data. - Enhance tests for RotationComposer to validate pull creation and power values. - Remove WorkoutCreator and its tests as part of the refactor.

---
+++ ZwiftTTTSim.Core/Model/Pull.cs
@@ -0,0 +1,24 @@
+
+namespace ZwiftTTTSim.Core.Model;
+
+/// <summary>
+/// Represents a rotation in a team time trial simulation.
+/// </summary>
+public class Pull
+{
+    /// <summary>
+    /// Gets or sets the pull number.
+    /// </summary>
+    public int PullNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the duration of the pull.
+    /// </summary>
+    public TimeSpan PullDuration { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of positions for this pull. This list contains the riders and their respective positions and target powers.
+    /// </summary>
+    public required List<PullPosition> PullPositions { get; init; }
+
+}
--- ZwiftTTTSim.Core/Model/RiderData.cs
+++ ZwiftTTTSim.Core/Model/RiderData.cs
@@ -1,9 +1,22 @@
 namespace ZwiftTTTSim.Core.Model;
 
+
+/// <summary>
+/// Represents the essential data for a rider in the simulation.
+/// </summary>
 public class RiderData
 {
+    /// <summary>
+    /// Gets or sets the rider's FTP (Functional Threshold Power).
+    /// </summary>
     public double FTP { get; set; }
+    /// <summary>
+    /// Gets or sets the rider's weight in kilograms.
+    /// </summary>
     public double Weight { get; set; }
 
+    /// <summary>
+    /// Gets the rider's power-to-weight ratio (W/kg).
+    /// </summary>
     public double WKg => FTP / Weight;
 }
